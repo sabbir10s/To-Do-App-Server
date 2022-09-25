cfa920095e0c0ff757b3f08a788686c8c72de568
sabbir10s <sabbirahmed1023@gmail.com>
2022-09-25 21:55:19 +0600

    task get, post, put and delete route added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.px8ct6k.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -14,11 +14,47 @@ async function run() {
     try {
         await client.connect();
         const taskCollection = client.db("ToDoApp").collection("task");
-        const task = { title: 'Book Reading', description: "To I will Read Book", status: "todo" }
-        const result = await taskCollection.insertOne(task)
-        console.log(result.insertedId);
 
-    } finally {
+        app.get('/task', async (req, res) => {
+            const query = {}
+            const task = await taskCollection.find(query).toArray();
+            res.send(task)
+        })
+
+
+        app.post('/task', async (req, res) => {
+            const newTask = req.body;
+            const result = await taskCollection.insertOne(newTask)
+            res.send(result)
+        })
+        app.put('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const { title, description, status } = req.body
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    title: title,
+                    description: description,
+                    status: status,
+                },
+            };
+
+            const result = await taskCollection.updateOne(query, updateDoc, options)
+            res.send(result)
+        })
+
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+
+
+    }
+    finally {
         //   await client.close();
     }
 }
